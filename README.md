e3225486af699f6c30d428adf3da316738d995ce
Cris <cristinamaillo.r@gmail.com>
2020-05-17 18:30:28 +0100

    finished css styling
    added contdown to model page

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # yogAI
 hi
+
+https://www.blobmaker.app/
+
+https://www.flaticon.com/free-icon/chakra_2043943?term=yoga&page=1&position=59
+https://www.flaticon.com/free-icon/sparkles_616493
--- modelDeploy.js
+++ modelDeploy.js
@@ -105,3 +105,26 @@ function draw() {
   textAlign(CENTER, CENTER);
   text(poseLabel, width / 2, height / 2);
 }
+
+function startTimer(duration, display) {
+  var timer = duration, minutes, seconds;
+  setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      display.textContent = minutes + ":" + seconds;
+
+      if (--timer < 0) {
+          timer = duration;
+      }
+  }, 1000);
+}
+
+window.onload = function () {
+  var thirtysecs = 60 * 0.5,
+      display = document.querySelector('#time');
+  startTimer(thirtysecs, display);
+};
